df01542507a80a250fcb89bfcba5477dd461af19
Fernando Gaol <ffilho@riolindo.org>
2020-01-24 22:20:30 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
-import PoemListPage from './PoemListPage'
+import PoemListPage from './PoemListPage';
 import './App.css';
+import PoemPage from './PoemPage';
+import { Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <nav>
-          <a href='/'>Poem List</a>
+          <Link to="/">Poem List</Link>
         </nav>
         <header>
           <h1>William Setstatespear</h1>
         </header>
         <main>
-          <PoemListPage />
+          <Route exact path="/" component={PoemListPage} />
+          <Route path="/poem/:poemId" component={PoemPage} />
         </main>
         <footer>
-          <p>© WilliamSetstatespear, 2018. All Rights Reserved.</p>
+          <p>© 2020. All Rights Reserved.</p>
         </footer>
       </div>
     );
